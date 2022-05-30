8601e973adce34bb503f33ee7db994ab67882dab
Payam Fakoorziba <payam.fakoorziba@mail.utoronto.ca>
2022-05-30 12:58:02 -0400

    pages

--- src/data/routes.js
+++ src/data/routes.js
@@ -12,5 +12,55 @@ export const ROUTES_SOURCE = [
     name: "about",
     component: () => import("@/views/about/index.vue"),
     meta: { title: "About" }
-  }
+  },
+  {
+    path: "/services",
+    label: "services",
+    name: "services",
+    component: () => import("@/views/services/index.vue"),
+    meta: { title: "Services" }
+  },
+  {
+    path: "/birds",
+    label: "birds",
+    name: "birds",
+    component: () => import("@/views/birds/index.vue"),
+    meta: { title: "Our Birds" }
+  },
+  {
+    path: "/industries",
+    label: "industries",
+    name: "industries",
+    component: () => import("@/views/industries/index.vue"),
+    meta: { title: "Industries" }
+  },
+  {
+    path: "/areas",
+    label: "areas",
+    name: "areas",
+    component: () => import("@/views/areas/index.vue"),
+    meta: { title: "Areas" }
+  },
+  {
+    path: "/faq",
+    label: "FAQ",
+    name: "faq",
+    component: () => import("@/views/faq/index.vue"),
+    meta: { title: "FAQ" }
+  },
+  {
+    path: "/blog",
+    label: "blog",
+    name: "blog",
+    component: () => import("@/views/blog/index.vue"),
+    meta: { title: "Blog" }
+  },
+  {
+    path: "/contact",
+    label: "contact",
+    name: "Contact",
+    component: () => import("@/views/contact/index.vue"),
+    meta: { title: "Contact Us" }
+  },
+
 ];
